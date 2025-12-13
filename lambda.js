f0636317126ca24b4d8228932f44a8851713d4ff
Abhishek069 <abhishinde0723@gmail.com>
2025-12-14 02:23:01 +0530

    update

--- lambda.js
+++ lambda.js
@@ -1,5 +1,5 @@
 import serverless from 'serverless-http';
-const app = require('./indexd.js');
+const app = require('./index.js');
 
 // Wrap the Express app with serverless-http
 export const handler = serverless(app);
